52c69cc4884418c83c5448d8953fdefd4f533ded
Timothy Chang <tchang135@gmail.com>
2022-12-04 18:39:57 -0500

    okay two sum done

--- TwoSum.rb
+++ TwoSum.rb
@@ -9,3 +9,25 @@ end
 
 # arr = [1, 2, 3, 4, 5, 0]
 # p bad_two_sum?(arr, 5)
+
+
+def okay_two_sum?(arr, target)
+    arr = arr.sort 
+    i = 0 
+    j = arr.length - 1 
+    until i >= arr.length 
+        while i < j 
+            if arr[i] + arr[j] == target 
+                return true 
+            else  
+                j -= 1 
+            end
+        end
+        i += 1 
+    end
+    false 
+end
+
+# arr = [1, 2, 1, 1, 4]
+# p okay_two_sum?(arr, 5)
+
